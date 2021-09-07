d952333ae95d4f7f7f962280f6eaa9aa5a967663
Mauro <maurogheco@gmail.com>
2021-09-07 17:31:19 -0300

    Se corrige el ordenador de productos para que priorice preferencia de usuario

--- src/turismotierramedia/Ordenar.java
+++ src/turismotierramedia/Ordenar.java
@@ -13,10 +13,10 @@ public class Ordenar implements Comparator<Producto> {
 	@Override
 	public int compare(Producto producto, Producto otroProducto) {
 		int aux;
-		aux = -producto.esUnaPromocion().compareTo(otroProducto.esUnaPromocion());
+		aux = producto.getTipoAtraccion().equals(tipoAtraccionUsuario) && !otroProducto.getTipoAtraccion().equals(tipoAtraccionUsuario) ? -1 
+				: !producto.getTipoAtraccion().equals(tipoAtraccionUsuario) && otroProducto.getTipoAtraccion().equals(tipoAtraccionUsuario) ? 1 : 0;
 		if(aux == 0) {
-			aux = producto.getTipoAtraccion().equals(tipoAtraccionUsuario) && !otroProducto.getTipoAtraccion().equals(tipoAtraccionUsuario) ? -1 
-					: !producto.getTipoAtraccion().equals(tipoAtraccionUsuario) && otroProducto.getTipoAtraccion().equals(tipoAtraccionUsuario) ? 1 : 0;
+			aux = -producto.esUnaPromocion().compareTo(otroProducto.esUnaPromocion());
 		}
 		if(aux == 0) {
 			aux = -producto.getCosto().compareTo(otroProducto.getCosto());
