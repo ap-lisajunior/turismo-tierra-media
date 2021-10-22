0eea68baba662c977eab6becd92e6ec60f02bdc4
Mauro <maurogheco@gmail.com>
2021-10-22 19:16:03 -0300

    Se agrega UsuarioDAOImpl

--- src/dao/UsuarioDAOImpl.java
+++ src/dao/UsuarioDAOImpl.java
@@ -6,34 +6,5 @@ import turismotierramedia.Usuario;
 
 public class UsuarioDAOImpl implements UsuarioDAO {
 
-	@Override
-	public List<Usuario> findAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int countAll() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int insert(Usuario t) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int update(Usuario t) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int delete(Usuario t) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
 
 }
